98e59ea836c0b69393f428923fdc5bd23cccc935
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-02 12:48:39 -0800

    server set up and running

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+// Write endpoints to perfom the following: 
+
+// POST 
+
+// GET - returns an array of all user objects
+
+// GET
+
+// DELETE 
+
+// PUT
+
+const port = 4000;
+server.listen(port, () =>
+  console.log(`\n ** API running on port ${port} **\n`)
+);
