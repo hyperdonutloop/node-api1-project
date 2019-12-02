73e7b80de7f79c11de004e0ec750dfcb27b88a3a
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-02 12:55:44 -0800

    returned all users and tested with insomnia

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const db = require('./data/db.js');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.send({ api: 'up and running...' });
+})
 // Write endpoints to perfom the following: 
 
 // POST 
-
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log('error on GET /users', error);
+      res.status(500)
+      .json({ errorMessage: 'error getting list of users' });
+    })
+})
 // GET - returns an array of all user objects
 
 // GET
