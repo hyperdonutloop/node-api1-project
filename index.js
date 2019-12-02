031cdd01f42c83fc633441ed64809a5611f2ae82
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-02 13:12:33 -0800

    returning user by id works. tested with insomnia

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ server.get('/', (req, res) => {
 // Write endpoints to perfom the following: 
 
 // POST 
+
+// GET - returns an array of all user objects
 server.get('/api/users', (req, res) => {
   db.find()
     .then(users => {
@@ -21,9 +23,22 @@ server.get('/api/users', (req, res) => {
       .json({ errorMessage: 'error getting list of users' });
     })
 })
-// GET - returns an array of all user objects
 
-// GET
+
+// GET - returns user object with specified id
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  
+  db.findById(id)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log('error on GET /users/:id', error);
+      res.status(500)
+      .json({ errorMessage: 'error getting list of users by id' });
+    })
+})
 
 // DELETE 
 
